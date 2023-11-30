3d1a6ac2067fc49aa7e04e3b16f35f05b272ade8
Oscar R Acuña <147223484+oscarracuna@users.noreply.github.com>
2023-11-30 01:35:03 -0700

    Update password_gen.py

--- password_gen.py
+++ password_gen.py
@@ -5,6 +5,12 @@ minus = mayus.lower()
 nums = "1234567890"
 spec_chars = "!@$%^&()=-_+;:',./"
 
+sec_tips = [
+            "remember to periodically update your passwords! :)","Add an extra layer of security by enabling 2FA when available. :)",
+            "Don't trust unsolicited messages or calls asking for personal information.", "Avoid connecting to public Wi-Fi for sensitive transactions.",
+            "Stay safe :)"
+            ]
+
 all = ""
 length = input("Choose the lenght of your password: ")
 length = int(length)
@@ -20,8 +26,11 @@ if lower:
 if digs:
     all += nums
 if specs:
-    spec_chars
+    all += spec_chars
 
 for x in range(amount):
     psw = "".join(random.sample(all, length))
     print(psw)
+    
+
+print(f'\n\nQuick security tip: {random.choice(sec_tips)}')
